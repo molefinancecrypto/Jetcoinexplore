936cd6a55d2188d99b62c826d6adbf304f587f6e
Dickson Joseph <ochekayjay@gmail.com>
2022-02-22 18:12:32 +0100

    edited the sponsorship banner

--- src/components/Sponsorship.js
+++ src/components/Sponsorship.js
@@ -6,7 +6,7 @@ import AliceCarousel from 'react-alice-carousel';
 function Sponsorship({news}) {
     
        const item = news.map(news => <div style={{height:'200px', width:'220px',backgroundColor:'rgb(0,0,24)',textAlign:'center'}}>
-           <div style={{display:'flex',alignItems:'center',justifyContent:'center',width:'90%',height:'80%'}}><img src={news['urlToImage']} style={{height:'90%', width:'90%'}}/></div>
+           <div style={{display:'flex',alignItems:'center',justifyContent:'center',width:'90%',height:'80%',margin:'0px auto'}}><img src={news['urlToImage']} style={{height:'90%', width:'100%'}}/></div>
            <p style={{color:"white"}}>{news['title']}</p>
        </div>)
 
@@ -20,7 +20,7 @@ const items = item;
          }
      }
 
-  return <div style={{backgroundColor:'#0f1c50',width:'60%',height:'240px',padding:'20px',boxSizing:"border-box",borderRadius:'10px',marginLeft:'20px'}}>
+  return <div style={{backgroundColor:'#0f1c50',width:'40%',height:'260px',padding:'20px',boxSizing:"border-box",borderRadius:'10px',marginLeft:'20px'}}>
       <AliceCarousel mouseTracking
       infinite
       autoPlayInterval={1000}
