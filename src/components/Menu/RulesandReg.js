2ee22d0299221a7904cf477c3490fb5cbd76088a
Dickson Joseph <ochekayjay@gmail.com>
2022-06-08 23:01:35 +0100

    newly recommended changes

--- src/components/Menu/RulesandReg.js
+++ src/components/Menu/RulesandReg.js
@@ -25,7 +25,7 @@ export default function RulesandReg({overallwidth}) {
       }
   }
   const dyor = ()=>{
-      setsliderposition({left:33.3,info:'none',contracts:'block',Link:'none',addons:'none'})
+      setsliderposition({left:66.6,info:'none',contracts:'block',Link:'none',addons:'none'})
       setheader('DYOR')
       if(overallwidth<=900){
           setheadertoshow(1)
@@ -33,8 +33,8 @@ export default function RulesandReg({overallwidth}) {
   }
 
   const pcpolicy = ()=>{
-    setsliderposition({left:66.6,info:'none',contracts:'block',Link:'none',addons:'none'})
-    setheader('Privacy & Cookie Policy')
+    setsliderposition({left:33.3,info:'none',contracts:'block',Link:'none',addons:'none'})
+    setheader('Privacy Policy')
     if(overallwidth<=900){
         setheadertoshow(2)
     }
@@ -45,12 +45,12 @@ export default function RulesandReg({overallwidth}) {
                 <div style={{color:"white",paddingLeft:'2.5%',width:"100%",justifyContent:"left",display:"flex",boxSizing:"border-box",cursor:'pointer',fontSize:'20px'}} onClick={()=> navigate('/')}><p>{backward}</p>Back</div>
                 <p className={overallwidth<700?'none':'enlist'}><strong>{header}</strong></p>
             </div>
-            <div style={{display:overallwidth<700?'flex':'none',justifyContent:'space-around',width:'95%',margin:'20px auto'}}><p onClick={()=>{setheadertoshow(0)}} className= {headertoshow===0?'mobilebuttonrandr':'mobilebuttonrandrgrey'}>Terms & Conditions</p><p onClick={()=>{setheadertoshow(1)}} className= {headertoshow===1?'mobilebuttonrandr':'mobilebuttonrandrgrey'}>DYOR</p><p onClick={()=>{setheadertoshow(2)}} className= {headertoshow===2?'mobilebuttonrandr':'mobilebuttonrandrgrey'}>Privacy Policy</p></div>
+            <div style={{display:overallwidth<700?'flex':'none',justifyContent:'space-around',width:'95%',margin:'20px auto'}}><p onClick={()=>{setheadertoshow(0)}} className= {headertoshow===0?'mobilebuttonrandr':'mobilebuttonrandrgrey'}>Terms & Conditions</p><p onClick={()=>{setheadertoshow(2)}} className= {headertoshow===2?'mobilebuttonrandr':'mobilebuttonrandrgrey'}>Privacy Policy</p><p onClick={()=>{setheadertoshow(1)}} className= {headertoshow===1?'mobilebuttonrandr':'mobilebuttonrandrgrey'}>DYOR</p></div>
             <div style={{height:overallwidth<=900?'600px':'800px',width:overallwidth<=900?'100%':'95%',margin:'20px auto',border:'1px solid #112836',backgroundImage: 'linear-gradient(90.18deg, #040B15 35.76%, rgba(6, 17, 31, 10) 110.28%)',borderRadius:'7px',padding:'20px',boxSizing:'border-box'}}>
                 {overallwidth<=700?<div style={{width:'100%',position:'relative',textAlign:'center',fontSize:'17px',marginBottom:'20px',paddingTop:'25px'}}>{headerList[headertoshow]} </div>:<div style={{display:'flex',position:'relative',border:'0.5px solid grey',borderWidth:'0px 0px 0.5px',paddingTop:'15px',paddingBottom:'15px',justifyContent:'space-around',height:'40px',alignItems:'center',width:'100%'}}>
                     <p style={{cursor:'pointer'}} onClick={ rules}>Terms & Conditions</p>
+                    <p style={{cursor:'pointer'}} onClick={ pcpolicy}>Privacy Policy</p>
                     <p  style={{cursor:'pointer'}} onClick={dyor}>DYOR</p>
-                    <p style={{cursor:'pointer'}} onClick={ pcpolicy}>Privacy & Cookie Policy</p>
                     <div style={{position:'absolute',backgroundColor:'#062750',width:'33.3%',height:'1px',top:'100%',left:`${sliderposition.left}%`}}></div>
                 </div>}
                 <div style={{width:'100%',marginTop:'40px'}}></div>
