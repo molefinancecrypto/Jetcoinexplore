39b061b171ef807bc402dec4bc671d7744de6a39
Dickson Joseph <ochekayjay@gmail.com>
2022-12-19 12:41:22 +0100

    added mock data  to test fully booked dates functionality

--- src/components/Menu/BannerpromoAds.js
+++ src/components/Menu/BannerpromoAds.js
@@ -56,6 +56,24 @@ export default function BannerpromoAds({overallwidth,leftholder,banasec}) {
         }
      }
 
+     const calendarObj = {month:[11,12,1],days:[3,14,7,27]}
+     //function to disable a day
+     const disableDay = ({ _, date, view }) => {
+      
+      // Check if a date React-Calendar wants to check is on the list of dates to add class to
+      //console.log(typeof(date.getDay()) )
+      
+    
+      if (calendarObj.month.includes(date.getMonth()+1)  && calendarObj.days.includes(date.getDate())){
+        return date.getDate()
+      }
+      //console.log(`the month is ${date.getMonth()}`)
+      //console.log('a')
+      //console.log(`the year is ${date.getFullYear()}`)
+      //console.log(date.getDay())}
+     }
+   
+
 
   useEffect(()=>{
     if(banasec && overallwidth>900){
@@ -78,7 +96,7 @@ export default function BannerpromoAds({overallwidth,leftholder,banasec}) {
         <div style={{width:overallwidth>900?'60%':'85%',border:'2px solid #0B1F36',padding:'10px 5px',borderRadius:'10px',margin:'10px auto',display:'flex',justifyContent:'space-around'}}><p>Ad redirect:</p> &nbsp; <input type='text' placeholder='https://tt.me/coinexplore' style={{backgroundColor:'transparent',width:'50%',fontSize:'13px',color:'white',outline:'none',borderWidth:'0px 0px 0px',letterSpacing:'1.5px'}}/></div>
         <p style={{width:'80%',margin:'5px auto',textAlign:'center',fontFamily:'NexaTextLight',fontSize:'15px'}}>Coin Unavailable? <NavLink to='/addcoin' style={{color:'rgb(176, 176, 240)'}}>Add here</NavLink></p>
         <p  style={{width:overallwidth>900?'50%':'70%',margin:'20px auto',textAlign:'center',marginBottom:'15px'}}>AVAILABLE SLOTS</p>
-        <Calendar onChange={setDate} value={date} onClickDay={clickday} tileClassName={tileClassName}/>
+        <Calendar onChange={setDate} value={date} onClickDay={clickday} tileClassName={tileClassName} tileDisabled={disableDay}/>
     </div>
   )
 }
